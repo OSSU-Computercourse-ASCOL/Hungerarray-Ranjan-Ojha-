071f908a0d9d7f459ad9a07e5ec0b5b3619907ab
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-10 00:19:01 +0545

    Problem set 1.3

--- 1.1/ps1.py
+++ 1.1/ps1.py
@@ -1,8 +1,16 @@
 s = 'azcbobobegghakl'
 
-total = 0
-for pos in range(len(s) - 2):
-    if "bob" == s[pos:pos+3]:
-        total += 1
-
-print ("Number of times bob occurs is:", total)
+curMax = 0
+Max = 0
+p = 0
+for pos in range(len(s) - 1):
+        if s[pos] <= s[pos + 1]:
+            curMax += 1
+        else:
+            curMax = 0
+        if Max < curMax:
+            Max = curMax
+            p = pos
+if p:
+    p += 1
+print (s[p-Max:p + 1]) 
