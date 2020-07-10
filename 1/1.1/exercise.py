0fad344810e67edd66723b3e9f93617118070de1
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-10 19:07:47 +0545

    Towers of Hanoi
    .... *sigh*
    To do : understand the code {else part}

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -1,18 +1,16 @@
 # --------------------------------------------
-# Exercise: (self) Real number to Binary
+# Exercise: Towers of Hanoi (Recursive function)
 # --------------------------------------------
 
-conv = float (input ("Number: "))
-
-p = 0
-while p != 17 and conv % 1 != 0:
-    p += 1
-    conv = conv * 2
-conv = int (conv)
-
-result = ''
-while conv > 0:
-    result = str(conv % 2) + result
-    conv //= 2
-print (result[:-p] + "." + result[-p:])
-
+def printMove (fr, to):
+    print ("move from " + str(fr) + " to " + str(to))
+    
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMove (fr, to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+        
+print (Towers (4, "p1", "p2", "p3"))
