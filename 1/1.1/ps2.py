3be02d12f7b8e6a3ef9d92d4ea7aff0cce28cde5
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-11 00:41:28 +0545

    PS2 : set 3

--- 1/1.1/ps2.py
+++ 1/1.1/ps2.py
@@ -1,25 +1,38 @@
 # -------------------------------------------------------------------------
-# set 2
+# set 3
 # -------------------------------------------------------------------------
 
-balance = 3329
-annualInterestRate = 0.2
+balance = 999999
+annualInterestRate = 0.18
 
 def currBal (bal, ir, mp):
     return (bal - mp) * (1 + ir / 12)
 
-payment = 10
+# if there were no interest on our sum then we would have to pay this much
+lwr = balance / 12
+
+# if we paid all at the end then we would pay this much it is larger simply because of the fact that we aren't paying anything at the end of the month
+upr = (balance * ( 1 + annualInterestRate/12) ** 12) / 12
+
+payment = (lwr + upr) / 2
+
 while True:
     mnth = 0
     testBalance = balance
+    
     while mnth < 12:
         mnth += 1
         testBalance = currBal(testBalance, annualInterestRate, payment)
-    if testBalance < 1:
-        break;
-    payment += 10
+    
+    if abs(testBalance - 0) < 0.0001:
+        break
+    elif testBalance < 0:
+        upr = payment
+    else:
+        lwr = payment
+    payment = (lwr + upr) / 2
 
-print ("Lowest Payment:", payment)
+print ("Lowest Payment:", round(payment, 2))
 
 
  
