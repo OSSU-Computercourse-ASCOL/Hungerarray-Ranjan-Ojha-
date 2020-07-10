93e325b3fc5096c6b6dd7b7903e5bb2d7227a019
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-10 22:09:10 +0545

    Palindrome Recursive (self)
    well I did it ... nothing else to add.

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -1,19 +1,50 @@
 # --------------------------------------------
-# Exercise: gcdRecur
+# Exercise: palindrome recursive
 # --------------------------------------------
 
-def gcdRecur(a, b):
-    '''
-    a, b: positive integers
+def palindrome (x):
+    """
+    Parameters
+    ----------
+    x : STRING
+
+    Returns bool
+    -------
+    the return value determines wheather the given string is palindrome or not.
+    """
+    def parse (x):
+        """
+        Parameters
+        ----------
+        x : string
+        Returns string
+        -------
+        parses the string to be fit for checking
+        """
+        parsed = ''
+        for ch in x:
+            if ch.isalpha():
+                parsed += ch
+        return parsed.lower()
     
-    returns: a positive integer, the greatest common divisor of a & b.
-    '''
-    if b == 0:
-        # gcd(a,0) = a
-        return a
-    else:
-        # gcd(a, b) = gcd(b, a Euler division b 
-        # attributed to gcd (a,b) = gcd(a, a-b)
-        return gcdRecur(b, a % b)
-    
-gcdRecur (100, 200)
+    def recur(x):
+        """
+        Parameters
+        ----------
+        x : string
+        Returns bool
+        -------
+        palindrome check
+        """
+        if len(x) == 0 or len(x) == 1:
+            return True
+        else:
+            if x[0] == x[- 1]:
+                return recur (x[1:-1])
+            else:
+                return False
+            
+    x = parse(x)
+    return recur(x)
+
+palindrome("Able was I, ere I saw Elba")
