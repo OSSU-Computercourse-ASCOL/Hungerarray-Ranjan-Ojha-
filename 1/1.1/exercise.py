cee2ed60fd2dfcac73c4a80f47cde17c607a8d46
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-10 22:21:10 +0545

    Exercise: check in sorted array

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -1,50 +1,26 @@
 # --------------------------------------------
-# Exercise: palindrome recursive
+# Exercise: check in sorted string
 # --------------------------------------------
 
-def palindrome (x):
-    """
-    Parameters
-    ----------
-    x : STRING
-
-    Returns bool
-    -------
-    the return value determines wheather the given string is palindrome or not.
-    """
-    def parse (x):
-        """
-        Parameters
-        ----------
-        x : string
-        Returns string
-        -------
-        parses the string to be fit for checking
-        """
-        parsed = ''
-        for ch in x:
-            if ch.isalpha():
-                parsed += ch
-        return parsed.lower()
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
     
-    def recur(x):
-        """
-        Parameters
-        ----------
-        x : string
-        Returns bool
-        -------
-        palindrome check
-        """
-        if len(x) == 0 or len(x) == 1:
-            return True
+    returns: True if char is in aStr; False otherwise
+    '''
+    if not len(aStr) or not len(char):
+        return False
+    
+    curr = len(aStr) // 2
+    if aStr[curr] == char:
+        return True
+    else:
+        if aStr[curr] < char:
+            isIn(char, aStr[:curr])
         else:
-            if x[0] == x[- 1]:
-                return recur (x[1:-1])
-            else:
-                return False
-            
-    x = parse(x)
-    return recur(x)
+            isIn(char, aStr[curr + 1:])
 
-palindrome("Able was I, ere I saw Elba")
+print(isIn("h","abcdhijkl"),
+isIn("c",""),
+isIn("","ahr"), isIn("",""))
