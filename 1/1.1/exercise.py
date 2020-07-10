fcf462d31577a9676c290efe4c0c1492891a4a92
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-11 03:48:56 +0545

    updates

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -1,62 +1,17 @@
-# --------------------------------------------
-# Exercise: word Frequency
-# --------------------------------------------
+def biggest(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
 
-def make_dict (data):
-    """
-    Parameters
-    ----------
-    data : string
-        a string containing all the words.
-    Returns : dictionary
-    -------
-    """
-    freq = {}
-    for word in data:
-        if word in freq:
-            freq[word] += 1
-        else:
-            freq[word] = 1
-    return freq
+    returns: The key with the largest number of values associated with it
+    '''
+    high = 0
+    for i in aDict.values():
+        if high < len(i):
+            high = len(i)
+    for elem in aDict:
+        if len(aDict[elem]) == high:
+            return elem
+    
+    return 
 
-def higest (dictionary):
-    '''
-    Parameters
-    ----------
-    dictionary : DICTIONARY
-        use the dictionary that has just been generated.
-    Returns : a tuple of list of word/s and their occurance
-    -------
-    '''
-    maxm = max(dictionary.values())
-    ls = []
-    for key in dictionary.keys():
-        if dictionary[key] == maxm:
-            ls.append(key)
-    if len(ls) == 1:
-        return (ls[0], maxm)
-    else:
-        return (ls, maxm)
-
-def atlest (dictionary, num):
-    '''
-    Parameters
-    ----------
-    dictionary : DICTIONARY
-        contains data
-    num : INT
-        the least no. of times you wish for it to occur
-    Returns a list of tuples, each tuple contains a list of words and their occurance
-    -------
-    '''
-    ls = []
-    for i in range(num,max(dictionary.values()) + 1):
-        
-        words = []
-        for word in dictionary:
-            if dictionary[word] == i:
-                words.append(word)
-        
-        ls.append((words, i))
-        
-        
+print (biggest({'b': [1, 7, 5, 4, 3, 18, 10, 0], 'a': []}))
