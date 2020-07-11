c2ecd5012a956b4d36c5a8cfcd96fb504d749118
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-12 02:38:19 +0545

    Class updated:
    added links to check out the methods available for classes as well.

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -2,13 +2,20 @@
 # Exercise: First Class
 #--------------------------------------------------------------------
 
-class Coordinat(object):
+class Coordinate(object):
+    # method to initialize the class
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
+    # self defined method
     def distance(self, other):
         x_diff_sq = (self.x - other.x) ** 2
         y_diff_sq = (self.y - other.y) ** 2
         return (x_diff_sq + y_diff_sq) ** .5
+    
+    # method to add objects of class
+    def __add__ (self, other):
+        return Coordinate(self.x + other.x, self.y + other.y)
+    
     
