d55cd26ea1ccf0faf3158cd402a95738da87aa1c
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-12 03:21:54 +0545

    Exercise : intSet

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -2,19 +2,45 @@
 # Exercise: coordinate exercise
 #--------------------------------------------------------------------
 
-class Coordinate(object):
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
 
-    def getX(self):
-        # Getter method for a Coordinate object's x coordinate.
-        # Getter methods are better practice than just accessing an attribute directly
-        return self.x
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
 
-    def getY(self):
-        # Getter method for a Coordinate object's y coordinate
-        return self.y
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self""" 
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
 
     def __str__(self):
-        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+    
+    def intersect(self, other):
+        """Returns a intSet that has integers that appear in both"""
+        result = intSet()
+        for i in self.vals:
+            if i in other.vals:
+                result.insert(i)
+        return result
+    
+    def __len__(self):
+        return len(self.vals)
