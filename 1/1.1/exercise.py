c9225bb13a28d8c5dc0b3bdeaa82fc1657c100d9
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-12 03:13:41 +0545

    Exercise coordinate

--- 1/1.1/exercise.py
+++ 1/1.1/exercise.py
@@ -1,21 +1,20 @@
 #--------------------------------------------------------------------
-# Exercise: First Class
+# Exercise: coordinate exercise
 #--------------------------------------------------------------------
 
 class Coordinate(object):
-    # method to initialize the class
-    def __init__(self, x, y):
+    def __init__(self,x,y):
         self.x = x
         self.y = y
-    
-    # self defined method
-    def distance(self, other):
-        x_diff_sq = (self.x - other.x) ** 2
-        y_diff_sq = (self.y - other.y) ** 2
-        return (x_diff_sq + y_diff_sq) ** .5
-    
-    # method to add objects of class
-    def __add__ (self, other):
-        return Coordinate(self.x + other.x, self.y + other.y)
-    
-    
+
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
