ff05f1cd9a03bcdcc744e2a3e898205c3db39259
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-11 23:30:30 +0545

    Problem Set 4: subset-6 implementing game with computer

--- 1/1.1/PS4/ps4b.py
+++ 1/1.1/PS4/ps4b.py
@@ -124,8 +124,60 @@ def playGame(wordList):
 
     wordList: list (string)
     """
-    # TO DO... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this when you code this function
+    flag = True
+    while True:
+        
+        # 1) Asks the user to input 'n' or 'r' or 'e'.
+        #     * If the user inputs 'e', immediately exit the game.
+        #     * If the user inputs anything that's not 'n', 'r', or 'e', keep asking them again.
+        hand = {}
+        while True:
+            try:
+                gameType = input(
+                    "Enter n to deal a new hand, r to replay the last hand, or e to end game: ").lower()
+                if gameType not in ['n', 'r', 'e']:
+                    raise Exception("Invalid command.")
+                if gameType == "e":
+                    flag = False
+                if gameType == 'r' and not len(hand):
+                    raise Exception("You have not played a hand yet. Please play a new hand first!")
+                break
+            except Exception as es:
+                print (es)
+        if not flag:
+             break
+        
+        
+        # 2) Asks the user to input a 'u' or a 'c'.
+        #     * If the user inputs anything that's not 'c' or 'u', keep asking them again.
+        while True:
+            try:
+                player = input(
+                    "Enter u to have yourself play, c to have the computer play: ").lower()
+                if player not in ['u', 'c']:
+                    raise Exception("Invalid command.")
+                break
+            except Exception as es:
+                print (es)
+            
+    
+        # 3) Switch functionality based on the above choices:
+        #     * If the user inputted 'n', play a new (random) hand.
+        #     * Else, if the user inputted 'r', play the last hand again.
+          
+        #     * If the user inputted 'u', let the user play the game
+        #       with the selected hand, using playHand.
+        #     * If the user inputted 'c', let the computer play the 
+        #       game with the selected hand, using compPlayHand.
+        if gameType == 'n':
+            hand = dealHand(HAND_SIZE)
+            
+        if player == 'u':
+            playHand(hand, wordList, HAND_SIZE)
+        elif player == 'c':
+            compPlayHand(hand.copy(), wordList, HAND_SIZE)
+            
+            
 
         
 #
