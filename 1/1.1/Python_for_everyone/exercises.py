81fd60081fa1ede4bca8284c0b27163df02411d5
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-14 03:39:24 +0545

    lesson 9: ex-5

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,8 +1,7 @@
-# Exercise 4: Add code to the previous program to figure out who has the
-# most messages in the file. After all the data has been read and the dic-
-# tionary has been created, look through the dictionary using a maximum
-# loop (see Chapter 5: Maximum and minimum loops) to find who has
-# the most messages and print how many messages the person has.
+# Exercise 5: This program records the domain name (instead of the
+# address) where the message was sent from instead of who the mail came
+# from (i.e., the whole email address). At the end of the program, print
+# out the contents of your dictionary.
 
 fname = input("Enter file name: ")
 try:
@@ -11,19 +10,12 @@ except FileNotFoundError:
     print ("%s : File not found" %fname)
     exit()
 
-emails = dict()
+domains = dict()
 for line in fhandle:
     if not line.startswith("From "): continue
 
     email = line.split()[1]
-    emails[email] = emails.get(email, 0) + 1
+    domain = email.split("@")[1]
+    domains[domain] = domains.get(domain, 0) + 1
 
-# added these
-mostEmail = None
-mostCount = None
-for email,count in emails.items():
-    if mostCount is None or mostCount < count: 
-        mostEmail = email
-        mostCount = count
-
-print (mostEmail, mostCount)
+print (domains)
