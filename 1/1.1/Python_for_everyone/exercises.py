520736b0b96ee55768cadc913f27e1f136252407
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-13 18:34:40 +0545

    lesson 7: ex-2

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,11 +1,27 @@
-# Exercise 1: Write a program to read through a file and print the contents
-# of the file (line by line) all in upper case. Executing the program will
-# look as follows:
+# Exercise 2: Write a program to prompt for a file name, and then read
+# through the file and look for lines of the form:
+# X-DSPAM-Confidence: 0.8475
+# When you encounter a line that starts with “X-DSPAM-Confidence:”
+# pull apart the line to extract the floating-point number on the line.
+# Count these lines and then compute the total of the spam confidence
+# values from these lines. When you reach the end of the file, print out
+# the average spam confidence.
 
-fname = input("Enter a file name: ")
-fhandle = open(fname, "r")
+try:
+    fname = input("Enter file name: ")
+    fhandle = open(fname, "r")
+except FileNotFoundError:
+    print ("Invalid File Name")
+    quit()
 
+lineCount = 0
+total = 0
 for line in fhandle:
-    line = line.rstrip()
-    print(line.upper())
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    strt = line.find(":")
+    total += float(line[strt + 1:])
+    lineCount += 1
+
+print ("Average spam confidence:", round(total/lineCount, 12))
 
