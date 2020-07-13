8285e995627102c612550fee2a8d64b996074758
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-14 00:27:00 +0545

    lesson 8: ex:5

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,25 +1,24 @@
-# Exercise 1: Write a function called chop that takes a list and modifies
-# it, removing the first and last elements, and returns None. Then write
-# a function called middle that takes a list and returns a new list that
-# contains all but the first and last elements.
+# Exercise 5: Write a program to read through the mail box data and
+# when you find line that starts with “From”, you will split the line into
+# words using the split function. We are interested in who sent the
+# message, which is the second word on the From line.
+# From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008
+# You will parse the From line and print out the second word for each
+# From line, then you will also count the number of From (not From:)
+# lines and print out a count at the end.
 
-def chop(ls):
-    ls.pop(0)
-    ls.pop(-1)
+try:
+    fname = input("Enter a file name: ")
+    fhandle = open (fname, "r")
+except FileNotFoundError:
+    print ("could not Open File", fname)
+    quit()
 
-def middle(ls):
-    return ls[1:-1]
+fromCount = 0
+for line in fhandle:
+    if not line.startswith("From "): continue
+    currLine = line.split()
+    print(currLine[1])
+    fromCount += 1
 
-from string import ascii_lowercase
-ls  = list(ascii_lowercase)
-
-
-print ("Before chop: \n", ls)
-chop(ls)
-print (ls)
-print ("After chop: \n", ls)
-
-ls = list(ascii_lowercase)
-print ("Before middle: \n", ls)
-print (middle(ls))
-print ("After middle: \n", ls)
+print ("There were %d lines in the file with From as the first word" %fromCount)
