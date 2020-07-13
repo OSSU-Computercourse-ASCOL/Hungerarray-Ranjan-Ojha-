6e7149d24f042fc98448fb6c16ad4d55fe18d17c
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-13 13:22:32 +0545

    lesson 6 - ex:5

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,22 +1,9 @@
-# Exercise 2: Write another program that prompts for a list of numbers
-# as above and at the end prints out both the maximum and minimum of
-# the numbers instead of the average.
+# Exercise 5: Take the following Python code that stores a string:
+# str = 'X-DSPAM-Confidence:0.8475'
+# Use find and string slicing to extract the portion of the string after the
+# colon character and then use the float function to convert the extracted
+# string into a floating point number.
 
-currMax = None
-currMin = None
-
-while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    try:
-        num = int(num)
-        if currMax is None or currMax < num:
-            currMax = num
-        if currMin is None or currMin > num:
-            currMin = num    
-    except ValueError:
-        print ("Invalid input")
-
-print ("Maximum is", currMax)
-print ("Minimum is", currMin)
+text = "X-DSPAM-Confidence:    0.8475"
+extract = text[text.find(":") + 1 : ]
+print (float(extract))
