2ac22ca3708e40b6a4511a114fb427f61effd02f
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-14 12:49:24 +0545

    lesson 10: ex-3

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,23 +1,25 @@
-# Exercise 2: This program counts the distribution of the hour of the day
-# for each of the messages. You can pull the hour from the “From” line
-# by finding the time string and then splitting that string into parts using
-# the colon character. Once you have accumulated the counts for each
-# hour, print out the counts, one per line, sorted by hour as shown below.
+# Exercise 3: Write a program that reads a file and prints the letters
+# in decreasing order of frequency. Your program should convert all the
+# input to lower case and only count the letters a-z. Your program should
+# not count spaces, digits, punctuation, or anything other than the letters
+# a-z. Find text samples from several different languages and see how
+# letter frequency varies between languages. Compare your results with
+# the tables at https://wikipedia.org/wiki/Letter_frequencies.
 
 fname = input("Enter a file name: ")
 try:
     fhandle = open(fname, "r")
 except FileNotFoundError:
-    print ("Did not find file %s" %fname)
+    print ("Could not open file %s" %fname)
     exit()
 
-# make a histogram for hour
-hourCount = dict()
+# make a dictionary by reading file for each letter
+letterCount = dict()
 for line in fhandle:
-    if not line.startswith("From "): continue
+    for letter in line.lower():
+        if not letter.isalpha(): continue
+        letterCount[letter] = letterCount.get(letter, 0) + 1
 
-    hour = line.split()[-2].split(":")[0]
-    hourCount[hour] = hourCount.get(hour, 0) + 1
-
-for (hour,count) in sorted(hourCount.items()):
-    print (hour, count)
+# print in descending order or letters frequency
+for elem in sorted(letterCount.items(), key= lambda letter: letter[1], reverse=True):
+    print (elem[0], elem[1])
