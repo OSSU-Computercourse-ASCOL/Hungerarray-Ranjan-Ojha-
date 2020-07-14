0f5bef0bd777d9632ab51217d3ea16ee22f9faad
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-14 12:17:47 +0545

    lesson 10: ex-1

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,21 +1,25 @@
-# Exercise 5: This program records the domain name (instead of the
-# address) where the message was sent from instead of who the mail came
-# from (i.e., the whole email address). At the end of the program, print
-# out the contents of your dictionary.
+# Exercise 1: Revise a previous program as follows: Read and parse the
+# “From” lines and pull out the addresses from the line. Count the num-
+# ber of messages from each person using a dictionary.
+# After all the data has been read, print the person with the most commits
+# by creating a list of (count, email) tuples from the dictionary. Then
+# sort the list in reverse order and print out the person who has the most
+# commits.
 
-fname = input("Enter file name: ")
+fname = input ("Enter a file name: ")
 try:
     fhandle = open(fname, "r")
 except FileNotFoundError:
-    print ("%s : File not found" %fname)
+    print ("%s Couldn't open the file." %fname)
     exit()
 
-domains = dict()
+# count the number of messages from each person using a dictionary
+messCount = dict()
 for line in fhandle:
     if not line.startswith("From "): continue
-
     email = line.split()[1]
-    domain = email.split("@")[1]
-    domains[domain] = domains.get(domain, 0) + 1
+    messCount[email] = messCount.get(email, 0) + 1
 
-print (domains)
+# list of count and email sorted in reverse order
+lst = sorted([(count, email) for (email, count) in messCount.items()], reverse=True)
+print (lst[0][1], lst[0][0])
