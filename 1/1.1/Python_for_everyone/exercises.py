0743ecda4b170bc11f00bc0167de6eff95bf7bea
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-07-16 03:03:53 +0545

    1.2 lesson 16: JSON parse->database (user, course, role)

--- 1/1.1/Python_for_everyone/exercises.py
+++ 1/1.1/Python_for_everyone/exercises.py
@@ -1,99 +1,67 @@
-# Musical Track Database
-# This application will read an iTunes export file in XML and produce a properly normalized database
+# Instructions
+# This application will read roster data in JSON format, parse the file, and then produce an SQLite database that contains a User, Course, and Member table and populate the tables from the data file.
 
-
-import xml.etree.ElementTree as ET
+import json
 import sqlite3
 
-conn = sqlite3.connect('data/trackdb.sqlite')
+conn = sqlite3.connect('data/rosterdb.sqlite')
 cur = conn.cursor()
 
-
-# Make some fresh tables using executescript()
+# Do some setup
 cur.executescript('''
-DROP TABLE IF EXISTS Artist;
-DROP TABLE IF EXISTS Genre;
-DROP TABLE IF EXISTS Album;
-DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
 
-CREATE TABLE Artist (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name    TEXT UNIQUE
+CREATE TABLE User (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name   TEXT UNIQUE
 );
 
-CREATE TABLE Genre (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name   TEXT UNIQUE 
+CREATE TABLE Course (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
 );
 
-CREATE TABLE Album (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    artist_id  INTEGER,
-    title   TEXT UNIQUE
-);
-
-CREATE TABLE Track (
-    id  INTEGER NOT NULL PRIMARY KEY 
-        AUTOINCREMENT UNIQUE,
-    title TEXT  UNIQUE,
-    album_id  INTEGER,
-    genre_id INTEGER,
-    len INTEGER, rating INTEGER, count INTEGER
-);
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
 ''')
 
-
 fname = input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'Library.xml'
+if len(fname) < 1:
+    fname = 'roster_data.json'
 
-# <key>Track ID</key><integer>369</integer>
-# <key>Name</key><string>Another One Bites The Dust</string>
-# <key>Artist</key><string>Queen</string>
-def lookup(d, key):
-    found = False
-    for child in d:
-        if found : return child.text
-        if child.tag == 'key' and child.text == key :
-            found = True
-    return None
+# [
+#   [ "Charley", "si110", 1 ],
+#   [ "Mea", "si110", 0 ],
 
-stuff = ET.parse(fname)
-all = stuff.findall('dict/dict/dict')
-print('Dict count:', len(all))
-for entry in all:
-    if ( lookup(entry, 'Track ID') is None ) : continue
+str_data = open(fname).read()
+json_data = json.loads(str_data)
 
-    name = lookup(entry, 'Name')
-    artist = lookup(entry, 'Artist')
-    album = lookup(entry, 'Album')
-    genre = lookup(entry, 'Genre')
-    count = lookup(entry, 'Play Count')
-    rating = lookup(entry, 'Rating')
-    length = lookup(entry, 'Total Time')
+for entry in json_data:
 
-    if name is None or artist is None or album is None or genre is None: 
-        continue
+    name = entry[0]
+    title = entry[1]
+    role = entry[2]
 
-    print(name, artist, album, genre, count, rating, length)
+    print((name, title, role))
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
-        VALUES ( ? )''', ( artist, ) )
-    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
-    artist_id = cur.fetchone()[0]
+    cur.execute('''INSERT OR IGNORE INTO User (name)
+        VALUES ( ? )''', ( name, ) )
+    cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
+    user_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
-        VALUES ( ?, ? )''', ( album, artist_id ) )
-    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
-    album_id = cur.fetchone()[0]
+    cur.execute('''INSERT OR IGNORE INTO Course (title)
+        VALUES ( ? )''', ( title, ) )
+    cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
+    course_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Genre (name)
-        VALUES ( ? )''', (genre, ))
-    cur.execute('SELECT ID FROM Genre WHERE name = ?', (genre, ))
-    genre_id = cur.fetchone()[0]
-
-    cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, genre_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ?, ? )''', 
-        ( name, album_id, genre_id, length, rating, count ) )
+    cur.execute('''INSERT OR REPLACE INTO Member
+        (user_id, course_id, role) VALUES ( ?, ? , ?)''',
+        ( user_id, course_id, role) )
 
     conn.commit()
